0d36bf136b3c1c4b003e5a69c0f278c678e4b216
Lucas Vieira <l_vieira@hotmail.com.br>
2025-08-09 18:40:33 -0300

    Removido a base do link

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,8 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
-const GITHUB_USER = 'lucas-kiozy';
-const REPO_NAME = 'espaco-origens-site';
-
 export default defineConfig({
-  site: `https://${GITHUB_USER}.github.io`,
-  base: `/${REPO_NAME}`, // caminho do projeto no Pages
-
+  site: 'https://espacoorigens.com.br', // domínio final
   integrations: [react()],
-
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  vite: { plugins: [tailwindcss()] }
 });
