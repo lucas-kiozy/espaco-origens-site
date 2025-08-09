7511cfc6552ffc48607dd6a7e660e17bada1e45b
Lucas Vieira <l_vieira@hotmail.com.br>
2025-08-09 18:21:23 -0300

    Publicar no GH pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
-
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
+const GITHUB_USER = 'lucas-kiozy';
+const REPO_NAME = 'espaco-origens-site';
+
 export default defineConfig({
+  site: `https://${GITHUB_USER}.github.io`,
+  base: `/${REPO_NAME}`, // caminho do projeto no Pages
+
   integrations: [react()],
 
   vite: {
