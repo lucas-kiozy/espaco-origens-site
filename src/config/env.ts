e926e0c94a6753ad8721103d0661ad73cfba0f8c
Lucas Vieira <l_vieira@hotmail.com.br>
2025-09-03 22:43:28 -0300

    Atualizado

--- src/config/env.ts
+++
@@ -1,7 +0,0 @@
-// Configuração de ambiente
-export const env = {
-  PUBLIC_SUPABASE_URL: 'https://cpjiclzbbquoulrykjgi.supabase.co',
-  PUBLIC_SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNwamljbHpiYnF1b3VscnlramdpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY4MzU0NTUsImV4cCI6MjA3MjQxMTQ1NX0.LQaKmPZa9Ywq_W9RS729ePk-BdUiFh_WPzOvw_J94fE',
-  PUBLIC_SITE_URL: 'https://lucas-kiozy.github.io/espaco-origens-site'
-}
-
