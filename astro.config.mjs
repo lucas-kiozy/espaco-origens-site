c73ef597f28d38ca7f44859d1e66f721b2adeb2a
Lucas Vieira <l_vieira@hotmail.com.br>
2025-09-03 23:29:15 -0300

    css corrigido

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,8 @@ import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  site: "https://lucas-kiozy.github.io",
+  site: "https://lucas-kiozy.github.io/espaco-origens-site",
   base: "/espaco-origens-site/",
   integrations: [react()],
-  vite: {
-    plugins: [tailwindcss()],
-  },
+  vite: { plugins: [tailwindcss()] },
 });
