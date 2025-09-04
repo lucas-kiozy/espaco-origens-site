93c78acf46354a56944d0c672ccfad0e9fc855e3
Lucas Vieira <48965967+lucas-kiozy@users.noreply.github.com>
2025-09-03 22:51:41 -0300

    Delete env.js

--- env.js
+++
@@ -1,4 +0,0 @@
-window.__ENV = {
-  PUBLIC_SUPABASE_URL: "'https://cpjiclzbbquoulrykjgi.supabase.co'",
-  PUBLIC_SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNwamljbHpiYnF1b3VscnlramdpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY4MzU0NTUsImV4cCI6MjA3MjQxMTQ1NX0.LQaKmPZa9Ywq_W9RS729ePk-BdUiFh_WPzOvw_J94fE"
-};
